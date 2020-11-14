d2217a09961fdffc7b26fa9f3fa06609e18c4af8
Marcell Kiss <marcellk.kiss@gmail.com>
2020-11-14 20:30:37 +0100

    Fix bug in arrayed reflection; update SPIRV-Cross

--- src/Program.cpp
+++ src/Program.cpp
@@ -83,16 +83,20 @@ void reflect_members(const spirv_cross::Compiler& refl, const spirv_cross::SPIRT
 		m.name = refl.get_member_name(type.self, i);
 		m.size = refl.get_declared_struct_member_size(type, i);
 		m.offset = refl.type_struct_member_offset(type, i);
+	
 		if (m.type == vuk::Program::Type::estruct) {
 			m.size = refl.get_declared_struct_size(spirtype);
 			reflect_members(refl, spirtype, m.members);
-		}
-		if (spirtype.array.size() > 0)
-			m.array_size = type.array[0];
-		else
-			m.array_size = 1;
-		members.push_back(m);
-	}
+        }
+
+        if(spirtype.array.size() > 0) {
+            m.array_size = spirtype.array[0];
+        } else {
+            m.array_size = 1;
+        }
+
+        members.push_back(m);
+    }
 }
 
 VkShaderStageFlagBits vuk::Program::introspect(const spirv_cross::Compiler& refl) {
