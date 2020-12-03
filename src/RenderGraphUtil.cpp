f4f33be54cab630e258f246698e5b33d1010b314
Marcell Kiss <marcellk.kiss@gmail.com>
2020-12-03 18:50:02 +0100

    Missing template<>

--- src/RenderGraphUtil.cpp
+++ src/RenderGraphUtil.cpp
@@ -30,21 +30,26 @@ namespace vuk {
 	using MP1 = MapProxy<Name, std::span<const UseRef>>;
 	using MPI1 = ConstMapIterator<Name, std::span<const UseRef>>;
 	using M1 = robin_hood::unordered_flat_map<Name, std::vector<UseRef, short_alloc<UseRef, 64>>>;
+
+	template<>
 	MP1::const_iterator MP1::cbegin() const noexcept {
 		auto& map = *reinterpret_cast<M1*>(_map);
 		return MP1::const_iterator(new M1::const_iterator(map.cbegin()));
 	}
 
+	template<>
 	MP1::const_iterator MP1::cend() const noexcept {
 		auto& map = *reinterpret_cast<M1*>(_map);
 		return MP1::const_iterator(new M1::const_iterator(map.cend()));
 	}
 
+	template<>
 	MP1::const_iterator MP1::find(Name key) const noexcept {
 		auto& map = *reinterpret_cast<M1*>(_map);
 		return MP1::const_iterator(new M1::const_iterator(map.find(key)));
 	}
 
+	template<>
 	size_t MP1::size() const noexcept {
 		auto& map = *reinterpret_cast<M1*>(_map);
 		return map.size();
@@ -55,21 +60,25 @@ namespace vuk {
 		delete reinterpret_cast<M1::const_iterator*>(_iter);
 	}
 
+	template<>
 	MPI1::ConstMapIterator(const MPI1& other) noexcept {
 		*reinterpret_cast<M1::const_iterator*>(_iter) = *reinterpret_cast<M1::iterator*>(other._iter);
 	}
 
+	template<>
 	MPI1::reference MPI1::operator*() noexcept {
 		const auto& iter = *reinterpret_cast<M1::const_iterator const*>(_iter);
 		std::pair<const Name&, std::span<const UseRef>> result(iter->first, std::span(iter->second));
 		return result;
 	}
 
+	template<>
 	MPI1& MPI1::operator++() noexcept {
 		reinterpret_cast<M1::iterator*>(_iter)->operator++();
 		return *this;
 	}
 
+	template<>
 	bool MPI1::operator==(MPI1 const& other) const noexcept {
 		return *reinterpret_cast<M1::iterator const*>(_iter) == *reinterpret_cast<M1::iterator const*>(other._iter);
 	}
@@ -79,21 +88,26 @@ namespace vuk {
 	using MP2 = MapProxy<Name, AttachmentRPInfo&>;
 	using MPI2 = ConstMapIterator<Name, AttachmentRPInfo&>;
 	using M2 = robin_hood::unordered_flat_map<Name, AttachmentRPInfo&>;
+
+	template<>
 	MP2::const_iterator MP2::cbegin() const noexcept {
 		auto& map = *reinterpret_cast<M2*>(_map);
 		return MP2::const_iterator(new M2::const_iterator(map.cbegin()));
 	}
 
+	template<>
 	MP2::const_iterator MP2::cend() const noexcept {
 		auto& map = *reinterpret_cast<M2*>(_map);
 		return MP2::const_iterator(new M2::const_iterator(map.cend()));
 	}
 
+	template<>
 	MP2::const_iterator MP2::find(Name key) const noexcept {
 		auto& map = *reinterpret_cast<M2*>(_map);
 		return MP2::const_iterator(new M2::const_iterator(map.find(key)));
 	}
 
+	template<>
 	size_t MP2::size() const noexcept {
 		auto& map = *reinterpret_cast<M2*>(_map);
 		return map.size();
@@ -104,21 +118,25 @@ namespace vuk {
 		delete reinterpret_cast<M2::const_iterator*>(_iter);
 	}
 
+	template<>
 	MPI2::ConstMapIterator(const MPI2& other) noexcept {
 		*reinterpret_cast<M2::const_iterator*>(_iter) = *reinterpret_cast<M2::iterator*>(other._iter);
 	}
 
+	template<>
 	MPI2::reference MPI2::operator*() noexcept {
 		const auto& iter = *reinterpret_cast<M2::const_iterator const*>(_iter);
 		std::pair<const Name&, AttachmentRPInfo&> result(iter->first, iter->second);
 		return result;
 	}
 
+	template<>
 	MPI2& MPI2::operator++() noexcept {
 		reinterpret_cast<M2::iterator*>(_iter)->operator++();
 		return *this;
 	}
 
+	template<>
 	bool MPI2::operator==(MPI2 const& other) const noexcept {
 		return *reinterpret_cast<M2::iterator const*>(_iter) == *reinterpret_cast<M2::iterator const*>(other._iter);
 	}
