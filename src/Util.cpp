faac5e61d9a23f95465506d83a5bdc229d85fef7
Marcell Kiss <marcellk.kiss@gmail.com>
2022-08-09 22:34:37 +0100

    test plumbing

--- src/Util.cpp
+++ src/Util.cpp
@@ -7,6 +7,7 @@
 #include <atomic>
 #include <mutex>
 #include <utility>
+#include <doctest/doctest.h>
 
 namespace vuk {
 	struct QueueImpl {
@@ -477,5 +478,16 @@ namespace vuk {
 			assert(0 && "not reached.");
 			return "";
 		}
+	}
+
+	int factorial(int number) {
+		return number <= 1 ? number : factorial(number - 1) * number;
+	}
+
+	TEST_CASE("testing the factorial function") {
+		CHECK(factorial(1) == 1);
+		CHECK(factorial(2) == 2);
+		CHECK(factorial(3) == 6);
+		CHECK(factorial(10) == 3628800);
 	}
 } // namespace vuk
