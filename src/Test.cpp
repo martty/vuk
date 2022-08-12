777ee9318e2747e1fc517977f5cfd58c7c7d1759
Marcell Kiss <marcellk.kiss@gmail.com>
2022-08-12 21:40:03 +0100

    more test plumbing

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,2 +1,9 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
+#ifdef VUK_BUILD_TESTS
+#define DOCTEST_CONFIG_IMPLEMENT
+#endif
+#include <doctest/doctest.h>
+#ifdef VUK_TEST_RUNNER
+int main(int argc, char** argv) {
+	return doctest::Context(argc, argv).run();
+}
+#endif
