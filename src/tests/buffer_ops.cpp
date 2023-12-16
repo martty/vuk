891ddcbb985b029ba3e05016c146b82b59674a11
Marcell Kiss <marcellk.kiss@gmail.com>
2023-12-16 11:45:35 +0000

    buffer2image2buffer

--- src/tests/buffer_ops.cpp
+++ src/tests/buffer_ops.cpp
@@ -36,7 +36,7 @@ TEST_CASE("test buffer upload/download") {
 	}
 }
 
-TEST_CASE("test buffer fill") {
+TEST_CASE("test buffer fill & update") {
 	REQUIRE(test_context.prepare());
 	{
 		auto data = { 0xfeu, 0xfeu, 0xfeu, 0xfeu };
@@ -50,10 +50,6 @@ TEST_CASE("test buffer fill") {
 		auto res = download_buffer(fill(declare_buf("src", **buf))).get(*test_context.allocator, test_context.compiler);
 		CHECK(std::span((uint32_t*)res->mapped_ptr, 4) == std::span(data));
 	}
-}
-
-TEST_CASE("test buffer update") {
-	REQUIRE(test_context.prepare());
 	{
 		std::array<const uint32_t, 4> data = { 0xfeu, 0xfeu, 0xfeu, 0xfeu };
 		auto buf = allocate_buffer(*test_context.allocator, { .mem_usage = MemoryUsage::eGPUonly, .size = sizeof(uint32_t) * 4 });
@@ -66,10 +62,10 @@ TEST_CASE("test buffer update") {
 		auto res = download_buffer(fill(declare_buf("src", **buf))).get(*test_context.allocator, test_context.compiler);
 		CHECK(std::span((uint32_t*)res->mapped_ptr, 4) == std::span(data));
 	}
-}
+}*/
 
 std::pair<Unique<Image>, TypedFuture<ImageAttachment>>
-create_image(Allocator& allocator, DomainFlagBits copy_domain, ImageAttachment ia, void* data, SourceLocationAtFrame loc = VUK_HERE_AND_NOW()) {
+create_image(Allocator& allocator, DomainFlagBits copy_domain, ImageAttachment ia, const void* data, SourceLocationAtFrame loc = VUK_HERE_AND_NOW()) {
 	auto image = allocate_image(allocator, ia, loc);
 	ia.image = **image;
 	return { std::move(*image), host_data_to_image(allocator, copy_domain, ia, data) };
@@ -82,7 +78,18 @@ create_image(Allocator& allocator, DomainFlagBits copy_domain, ImageAttachment i
 }
 
 auto image2buf = make_pass("copy image to buffer", [](CommandBuffer& cbuf, VUK_IA(Access::eTransferRead) src, VUK_BA(Access::eTransferWrite) dst) {
-	cbuf.copy_image_to_buffer(src, dst, );
+	BufferImageCopy bc;
+	bc.imageOffset = { 0, 0, 0 };
+	bc.bufferRowLength = 0;
+	bc.bufferImageHeight = 0;
+	bc.imageExtent = static_cast<Extent3D>(src->extent.extent);
+	bc.imageSubresource.aspectMask = format_to_aspect(src->format);
+	bc.imageSubresource.mipLevel = src->base_level;
+	bc.imageSubresource.baseArrayLayer = src->base_layer;
+	assert(src->layer_count == 1); // unsupported yet
+	bc.imageSubresource.layerCount = src->layer_count;
+	bc.bufferOffset = dst->offset;
+	cbuf.copy_image_to_buffer(src, dst, bc);
 	return dst;
 });
 
@@ -90,17 +97,16 @@ TEST_CASE("test image upload/download") {
 	REQUIRE(test_context.prepare());
 	{
 		auto data = { 1u, 2u, 3u, 4u };
-		auto ia = ImageAttachment::from_preset(ImageAttachment::Preset::eMap2D, Format::eR32Uint, { 2, 2, 1 }, Samples::e1);
+		auto ia = ImageAttachment::from_preset(ImageAttachment::Preset::eGeneric2D, Format::eR32Uint, { 2, 2, 1 }, Samples::e1);
 		auto [img, fut] = create_image(*test_context.allocator, DomainFlagBits::eAny, ia, std::span(data));
 
-		auto res = download_buffer(fut).get(*test_context.allocator, test_context.compiler);
-		CHECK(std::span((uint32_t*)res->mapped_ptr, 3) == std::span(data));
-	}
-	{
-		auto data = { 1u, 2u, 3u, 4u, 5u };
-		auto [buf, fut] = create_buffer(*test_context.allocator, MemoryUsage::eGPUonly, DomainFlagBits::eAny, std::span(data));
-
-		auto res = download_buffer(fut).get(*test_context.allocator, test_context.compiler);
-		CHECK(std::span((uint32_t*)res->mapped_ptr, 5) == std::span(data));
+		size_t alignment = format_to_texel_block_size(fut->format);
+		assert(fut->extent.sizing == Sizing::eAbsolute);
+		size_t size = compute_image_size(fut->format, static_cast<Extent3D>(fut->extent.extent));
+		auto dst = *allocate_buffer(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, size, alignment });
+		auto dst_buf = declare_buf("dst", *dst);
+		auto res = download_buffer(image2buf(fut, dst_buf)).get(*test_context.allocator, test_context.compiler);
+		auto updata = std::span((uint32_t*)res->mapped_ptr, 4);
+		CHECK(updata == std::span(data));
 	}
 }
