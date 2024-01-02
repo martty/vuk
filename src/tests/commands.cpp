f83219fcc653d73962b36adf8eeb66ca3255d59d
Marcell Kiss <marcellk.kiss@gmail.com>
2024-01-02 21:42:54 +0000

    change Future release semantics, 2 tests

--- src/tests/commands.cpp
+++ src/tests/commands.cpp
@@ -146,4 +146,80 @@ TEST_CASE("image blit") {
 	}
 }
 
+TEST_CASE("multi return pass") {
+	{
+		auto buf0 = allocate_buffer(*test_context.allocator, { .mem_usage = MemoryUsage::eGPUonly, .size = sizeof(uint32_t) * 4 });
+		auto buf1 = allocate_buffer(*test_context.allocator, { .mem_usage = MemoryUsage::eGPUonly, .size = sizeof(uint32_t) * 4 });
+		auto buf2 = allocate_buffer(*test_context.allocator, { .mem_usage = MemoryUsage::eGPUonly, .size = sizeof(uint32_t) * 4 });
+
+		auto fills = make_pass(
+		    "fills", [](CommandBuffer& cbuf, VUK_BA(Access::eTransferWrite) dst0, VUK_BA(Access::eTransferWrite) dst1, VUK_BA(Access::eTransferWrite) dst2) {
+			    cbuf.fill_buffer(dst0, 0xfc);
+			    cbuf.fill_buffer(dst1, 0xfd);
+			    cbuf.fill_buffer(dst2, 0xfe);
+			    return std::tuple{ dst0, dst1, dst2 };
+		    });
+
+		auto [buf0p, buf1p, buf2p] = fills(declare_buf("src0", **buf0), declare_buf("src1", **buf1), declare_buf("src2", **buf2));
+		{
+			auto data = { 0xfcu, 0xfcu, 0xfcu, 0xfcu };
+			auto res = download_buffer(buf0p).get(*test_context.allocator, test_context.compiler);
+			CHECK(std::span((uint32_t*)res->mapped_ptr, 4) == std::span(data));
+		}
+		{
+			auto data = { 0xfdu, 0xfdu, 0xfdu, 0xfdu };
+			auto res = download_buffer(buf1p).get(*test_context.allocator, test_context.compiler);
+			CHECK(std::span((uint32_t*)res->mapped_ptr, 4) == std::span(data));
+		}
+		{
+			auto data = { 0xfeu, 0xfeu, 0xfeu, 0xfeu };
+			auto res = download_buffer(buf2p).get(*test_context.allocator, test_context.compiler);
+			CHECK(std::span((uint32_t*)res->mapped_ptr, 4) == std::span(data));
+		}
+	}
+}
+
+#include <string>
+
+TEST_CASE("scheduling single-queue") {
+	{
+		std::string execution;
+		
+		auto buf0 = allocate_buffer(*test_context.allocator, { .mem_usage = MemoryUsage::eGPUonly, .size = sizeof(uint32_t) * 4 });
+		
+		auto write = make_pass("write", [&](CommandBuffer& cbuf, VUK_BA(Access::eTransferWrite) dst) {
+			execution += "w";
+			    return dst;
+		    });
+		auto read = make_pass("read", [&](CommandBuffer& cbuf, VUK_BA(Access::eTransferRead) dst) {
+			execution += "r";
+			return dst;
+		});
+
+		{
+			auto b0 = declare_buf("src0", **buf0);
+			write(write(b0)).wait(*test_context.allocator, test_context.compiler);
+			CHECK(execution == "ww");
+			execution = "";
+		}
+		{
+			auto b0 = declare_buf("src0", **buf0);
+			read(write(b0)).wait(*test_context.allocator, test_context.compiler);
+			CHECK(execution == "wr");
+			execution = "";
+		}
+		{
+			auto b0 = declare_buf("src0", **buf0);
+			write(read(write(b0))).wait(*test_context.allocator, test_context.compiler);
+			CHECK(execution == "wrw");
+			execution = "";
+		}
+		{
+			auto b0 = declare_buf("src0", **buf0);
+			write(read(read(write(b0)))).wait(*test_context.allocator, test_context.compiler);
+			CHECK(execution == "wrrw");
+		}
+	}
+}
+
 // TEST TODOS: image2image copy, resolve
