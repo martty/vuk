5e832891616bb22f8949f77cb46d0c0013baa4c0
Marcell Kiss <marcellk.kiss@gmail.com>
2024-01-26 23:12:42 +0000

    ex 6

--- examples/06_msaa.cpp
+++ examples/06_msaa.cpp
@@ -17,7 +17,9 @@ namespace {
 	float angle = 0.f;
 	auto box = util::generate_cube();
 	vuk::Unique<vuk::Buffer> verts, inds;
-	std::optional<vuk::Texture> texture_of_doge;
+	vuk::Unique<vuk::Image> image_of_doge;
+	vuk::Unique<vuk::ImageView> image_view_of_doge;
+	vuk::ImageAttachment texture_of_doge;
 
 	vuk::Example x{
 		.name = "06_msaa",
@@ -36,9 +38,13 @@ namespace {
 		      int x, y, chans;
 		      auto doge_image = stbi_load((root / "examples/doge.png").generic_string().c_str(), &x, &y, &chans, 4);
 
-		      auto [tex, tex_fut] = create_texture(allocator, vuk::Format::eR8G8B8A8Srgb, vuk::Extent3D{ (unsigned)x, (unsigned)y, 1u }, doge_image, false);
-		      texture_of_doge = std::move(tex);
-		      runner.enqueue_setup(std::move(tex_fut));
+		      texture_of_doge = vuk::ImageAttachment::from_preset(
+		          vuk::ImageAttachment::Preset::eMap2D, vuk::Format::eR8G8B8A8Srgb, vuk::Extent3D{ (unsigned)x, (unsigned)y, 1u }, vuk::Samples::e1);
+		      texture_of_doge.level_count = 1;
+		      auto [image, view, future] = vuk::create_image_and_view_with_data(allocator, vuk::DomainFlagBits::eTransferOnTransfer, texture_of_doge, doge_image);
+		      image_of_doge = std::move(image);
+		      image_view_of_doge = std::move(view);
+		      runner.enqueue_setup(future.as_released(vuk::Access::eFragmentSampled, vuk::DomainFlagBits::eGraphicsQueue));
 		      stbi_image_free(doge_image);
 
 		      // We set up the cube data, same as in example 02_cube
@@ -52,7 +58,7 @@ namespace {
 		      runner.enqueue_setup(std::move(ind_fut));
 		    },
 		.render =
-		    [](vuk::ExampleRunner& runner, vuk::Allocator& frame_allocator, vuk::Future target) {
+		    [](vuk::ExampleRunner& runner, vuk::Allocator& frame_allocator, vuk::Value<vuk::ImageAttachment> target) {
 		      struct VP {
 			      glm::mat4 view;
 			      glm::mat4 proj;
@@ -64,57 +70,60 @@ namespace {
 		      auto [buboVP, uboVP_fut] = create_buffer(frame_allocator, vuk::MemoryUsage::eCPUtoGPU, vuk::DomainFlagBits::eTransferOnGraphics, std::span(&vp, 1));
 		      auto uboVP = *buboVP;
 
-		      vuk::RenderGraph rg("06");
-		      rg.attach_in("06_msaa", std::move(target));
 		      // The rendering pass is unchanged by going to multisampled,
 		      // but we will use an offscreen multisampled color attachment
-		      rg.add_pass({ .resources = { "06_msaa_MS"_image >> vuk::eColorWrite, "06_msaa_depth"_image >> vuk::eDepthStencilRW },
-		                    .execute = [uboVP](vuk::CommandBuffer& command_buffer) {
-			                    command_buffer.set_viewport(0, vuk::Rect2D::framebuffer())
-			                        .set_scissor(0, vuk::Rect2D::framebuffer())
-			                        .set_rasterization({}) // Set the default rasterization state
-			                        // Set the depth/stencil state
-			                        .set_depth_stencil(vuk::PipelineDepthStencilStateCreateInfo{
-			                            .depthTestEnable = true,
-			                            .depthWriteEnable = true,
-			                            .depthCompareOp = vuk::CompareOp::eLessOrEqual,
-			                        })
-			                        .broadcast_color_blend({}) // Set the default color blend state
-			                        .bind_vertex_buffer(0,
-			                                            *verts,
-			                                            0,
-			                                            vuk::Packed{ vuk::Format::eR32G32B32Sfloat,
-			                                                         vuk::Ignore{ offsetof(util::Vertex, uv_coordinates) - sizeof(util::Vertex::position) },
-			                                                         vuk::Format::eR32G32Sfloat })
-			                        .bind_index_buffer(*inds, vuk::IndexType::eUint32)
-			                        .bind_image(0, 2, *texture_of_doge->view)
-			                        .bind_sampler(0, 2, {})
-			                        .bind_graphics_pipeline("textured_cube")
-			                        .bind_buffer(0, 0, uboVP);
-			                    glm::mat4* model = command_buffer.map_scratch_buffer<glm::mat4>(0, 1);
-			                    *model = static_cast<glm::mat4>(glm::angleAxis(glm::radians(angle), glm::vec3(0.f, 1.f, 0.f)));
-			                    command_buffer.draw_indexed(box.second.size(), 1, 0, 0, 0);
-		                    } });
+		      auto ms_render =
+		          vuk::make_pass("06_msaa_render", [uboVP](vuk::CommandBuffer& command_buffer, VUK_IA(vuk::eColorWrite) color, VUK_IA(vuk::eDepthStencilRW) depth) {
+			          command_buffer.set_viewport(0, vuk::Rect2D::framebuffer())
+			              .set_scissor(0, vuk::Rect2D::framebuffer())
+			              .set_rasterization({}) // Set the default rasterization state
+			              // Set the depth/stencil state
+			              .set_depth_stencil(vuk::PipelineDepthStencilStateCreateInfo{
+			                  .depthTestEnable = true,
+			                  .depthWriteEnable = true,
+			                  .depthCompareOp = vuk::CompareOp::eLessOrEqual,
+			              })
+			              .broadcast_color_blend({}) // Set the default color blend state
+			              .bind_vertex_buffer(0,
+			                                  *verts,
+			                                  0,
+			                                  vuk::Packed{ vuk::Format::eR32G32B32Sfloat,
+			                                               vuk::Ignore{ offsetof(util::Vertex, uv_coordinates) - sizeof(util::Vertex::position) },
+			                                               vuk::Format::eR32G32Sfloat })
+			              .bind_index_buffer(*inds, vuk::IndexType::eUint32)
+			              .bind_image(0, 2, *image_view_of_doge)
+			              .bind_sampler(0, 2, {})
+			              .bind_graphics_pipeline("textured_cube")
+			              .bind_buffer(0, 0, uboVP);
+			          glm::mat4* model = command_buffer.scratch_buffer<glm::mat4>(0, 1);
+			          *model = static_cast<glm::mat4>(glm::angleAxis(glm::radians(angle), glm::vec3(0.f, 1.f, 0.f)));
+			          command_buffer.draw_indexed(box.second.size(), 1, 0, 0, 0);
 
+					  return color;
+		          });
 		      angle += 180.f * ImGui::GetIO().DeltaTime;
 
 		      // We mark our MS attachment as multisampled (8 samples)
 		      // Since resolving requires equal sized images, we can actually infer the size of the MS attachment
 		      // from the final image, and we don't need to specify here
 		      // We use the swapchain format, since resolving needs identical formats
-		      rg.attach_and_clear_image(
-		          "06_msaa_MS", { .format = runner.swapchain->format, .sample_count = vuk::Samples::e8 }, vuk::ClearColor{ 0.f, 0.f, 0.f, 0.f });
-		      rg.attach_and_clear_image("06_msaa_depth", { .format = vuk::Format::eD32Sfloat }, vuk::ClearDepthStencil{ 1.0f, 0 });
-		      // We mark our final result "06_msaa_final" attachment to be a result of a resolve from "06_msaa_MS"
-		      rg.resolve_resource_into("06_msaa", "06_msaa_final", "06_msaa_MS+");
+		      auto ms_img = vuk::declare_ia("06_ms");
+		      ms_img->sample_count = vuk::Samples::e8;
+		      ms_img = vuk::clear_image(std::move(ms_img), vuk::ClearColor{ 0.f, 0.f, 0.f, 0.f });
+		      
+			  auto depth_img = vuk::declare_ia("06_depth");
+		      depth_img->format = vuk::Format::eD32Sfloat;
+		      depth_img = vuk::clear_image(std::move(depth_img), vuk::ClearDepthStencil{ 1.0f, 0 });
 
-		      return vuk::Future{ std::make_unique<vuk::RenderGraph>(std::move(rg)), "06_msaa_final" };
+		      // We mark our final result "06_msaa_final" attachment to be a result of a resolve from "06_msaa_MS"
+		      return vuk::resolve_into(ms_render(std::move(ms_img), std::move(depth_img)), std::move(target));
 		    },
 		.cleanup =
 		    [](vuk::ExampleRunner& runner, vuk::Allocator& frame_allocator) {
 		      verts.reset();
 		      inds.reset();
-		      texture_of_doge.reset();
+		      image_of_doge.reset();
+		      image_view_of_doge.reset();
 		    }
 
 	};
