9e8510a57000d6b946cc810dcad48dc6a1c03bfc
Marcell Kiss <marcellk.kiss@gmail.com>
2025-07-08 21:58:23 +0100

    reorder backtrace for error

--- src/runtime/vk/Backend.cpp
+++ src/runtime/vk/Backend.cpp
@@ -52,10 +52,10 @@ namespace vuk {
 	std::string format_source_location(Node* node) {
 		if (node->debug_info) {
 			std::string msg = "";
-			for (int i = (int)node->debug_info->trace.size() - 1; i >= 0; i--) {
+			for (int i = 0; i < node->debug_info->trace.size(); i++) {
 				auto& source = node->debug_info->trace[i];
 				msg += fmt::format("{}({}): ", source.file_name(), source.line());
-				if (i > 0) {
+				if (i < (node->debug_info->trace.size() - 1)) {
 					msg += "\n";
 				}
 			}
